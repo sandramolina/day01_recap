3ebc8561d7a2258fa15af2da7a6115ead369535e
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-03-28 16:12:37 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,7 @@
+# * In the file, create a simple Python program:
+
+#   * create variables of different types
+#   * call some methods/functions on these variables
+#   * use some operators (e.g. +, -)
+
+
